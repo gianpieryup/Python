a2dcf1bb3931301548a07c232a6d5a925975c242
gianpieryup <gyupanquisalvatierra@gmail.com>
2022-04-19 22:31:47 -0300

    update

--- rename_extension.py
+++ rename_extension.py
@@ -2,8 +2,8 @@ import os
 ext = input("Extension nueva ?: ")
 lista_archivos = os.listdir()
 for a in lista_archivos:
-    name,extension = a.split('.')
-    if extension != 'py':
+    name,extension = os.path.splitext(a)
+    if extension != '.py':
         new_name = name + '.' + ext
         #print(name,extension,new_name)
         os.rename(a,new_name)
